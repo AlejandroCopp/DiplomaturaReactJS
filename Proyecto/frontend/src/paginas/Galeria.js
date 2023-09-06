3a4634a3c311dc64e0ad35fc695e98eab8521802
AlejandroCopp <coppolerlucas@gmail.com>
2023-09-06 19:26:42 -0300

    agregue al texto de comandos git las credenciales

--- Proyecto/frontend/src/paginas/Galeria.js
+++ Proyecto/frontend/src/paginas/Galeria.js
@@ -5,10 +5,22 @@ const Galeria = (props) => {
         <div>
             <h2 className="text-center py-5 display-6">Galeria</h2>
             <div class="container" >
+             <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                 <img src="/img/Galeria/gal1.png" class="img-fluid " alt="..." />
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                 <img src="/img/Galeria/gal2.png" class="img-fluid " alt="..." />
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                 <img src="/img/Galeria/gal3.png" class="img-fluid " alt="..." />
+                </div>
+            </div>
+               
+               
                 <div class="galeria">
-                    <img src="/img/Galeria/gal1.png" class="img-fluid " alt="..." />
-                    <img src="/img/Galeria/gal2.png" class="img-fluid " alt="..." />
-                    <img src="/img/Galeria/gal3.png" class="img-fluid " alt="..." />
+                    
+                  
                     <img src="/img/Galeria/gal4.png" class="img-fluid " alt="..." />
                     <img src="/img/Galeria/gal5.png" class="img-fluid " alt="..." />
                     <img src="/img/Galeria/gal6.png" class="img-fluid " alt="..." />
